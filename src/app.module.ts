70b3fcb4e424d76b8b7f00451524c3d9dd628437
Christina Gu <yuxingu@christinas-air.milton.edu>
2023-02-03 15:21:33 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/products/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from './products.model';
+
+@Injectable()
+export class ProductsService{
+    private products: Product[] = [];
+
+    insertProduct(title: string, desc: string, price: number) {
+        const prodId = new Date().toString();
+        const newProduct = new Product (prodId, title, desc, price);
+        this.products.push(newProduct);
+        return prodId;
+    }
+
+    getProducts(){
+        return [...this.products];
+    }
+}
